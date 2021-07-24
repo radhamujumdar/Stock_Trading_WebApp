e2b62af0f44488f29874920494ecd07abcb1c444
radhamujumdar <radhamujumdar@gmail.com>
2021-07-24 15:31:03 +0530

    SCrollbar added and Navbar styling issue fixed.

--- src/login.js
+++ src/login.js
@@ -38,7 +38,7 @@ const Login = () => {
 										<i className="fas fa-lock"></i>
 										<input type="password" placeholder="Password" onChange={(e) =>setForm({...form, password: e.target.value})}/>
 									</div>
-									<button class="btn solid">Login</button>
+									<button class="btn1 solid">Login</button>
 									<p class="social-text">Or Sign In with Social Platforms</p>
 									<div class="social-media">
 										<a href="#" class="social-icon">
