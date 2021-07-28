f3937def16d06766aebe602f2f08129626dfb03b
Rutuja-Deshmukh0519 <60124635+Rutuja-Deshmukh0519@users.noreply.github.com>
2021-07-28 16:43:22 +0530

    Added Parameters

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,20 @@
 // import firebaseConfig from '../config';
-import firebase from 'firebase/app';
-import "firebase/auth";
-  
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyACHjaFUceqjdygpbg8zJYvHh69Jb-oOR8",
   authDomain: "stock-trading-app-e083f.firebaseapp.com",
+  databaseURL: "https://stock-trading-app-e083f-default-rtdb.firebaseio.com",
   projectId: "stock-trading-app-e083f",
   storageBucket: "stock-trading-app-e083f.appspot.com",
   messagingSenderId: "214687842762",
   appId: "1:214687842762:web:57de79a82b2c84997fcf33"
 };
-  
-firebase.initializeApp(firebaseConfig); 
-export const auth = firebase.auth();
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+export default firebase;
